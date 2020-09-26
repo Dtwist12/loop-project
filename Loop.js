bed51adf5820b6f11b5b2efa880c0e7c7aeb7b6a
Joy Gogna <70154253+Dtwist12@users.noreply.github.com>
2020-09-26 09:25:04 -0400

    Add files via upload

---
+++ Loop.js
@@ -0,0 +1,20 @@
+for(let i = 1000; i > 0; i-=2){
+    
+       console.log(i);
+}
+
+for( let i= 0; i< 10000; i++) {
+ if (i=2500){
+     alert('Quarter of the way there')
+
+     
+ }   if (i=5000) {
+     alert('Halfway there')
+
+     
+ } 
+        if (i=10000) {
+            alert('The loop is done')
+            
+        }
+    }
