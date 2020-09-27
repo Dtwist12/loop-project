b5d28fcd382f9b3f98bc0761ec857ca2e3727898
Joy Gogna <70154253+Dtwist12@users.noreply.github.com>
2020-09-27 12:50:16 -0400

    Update Loop.js

--- Loop.js
+++ Loop.js
@@ -17,4 +17,18 @@ for( let i= 0; i< 10000; i++) {
             alert('The loop is done')
             
         }
-    }
+    
+var Favoriteshows = ["Golden Girls", "Madea", "Jimmy neutron", "Looney Tunes", "Tom & Jerry"];
+
+for(let i = 0; i < Favoriteshows.length; i++) 
+  if(Favoriteshows[i] === "Golden Girls") {
+      console.log("My Favorite #1 Tv show is " + Favoriteshows[0]);
+  } else if (Favoriteshows[i] === "Madea") {
+      console.log("My Favorite #2 Tv show is " + Favoriteshows[1]);
+  } else if (Favoriteshows[i] === "Jimmy neutron"){
+      console.log("My Favorite #3 Tv show is " + Favoriteshows[2]);
+  } else if(Favoriteshows[i] === "Looney Tunes"){
+      console.log("My Favorite #4 Tv show is " + Favoriteshows[3]);
+  } else if(Favoriteshows[i] === "Tom & Jerry"){
+      console.log("My Favorite #5 Tv show is " + Favoriteshows[4]);
+  }
